7f8bf224e7f8fb10fbf1d467789826fcd06962e9
venmos <venmos@fuck.gfw.es>
2017-05-07 20:00:55 +0800

    first commit

---
+++ packages.el
@@ -0,0 +1,21 @@
+;;; packages.el --- venmos-config layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Venom
+;; Email:  Venom@fuck.gfw.es
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Code:
+
+(defconst venmos-config-packages
+  '())
+
+(setq-default spacemacs-banner-official-png "~/.emacs.d/private/venmos-config/spacemacs.png")
+(setq-default spacemacs-buffer-logo-title "[V E N O M - S P A C E M A C S]")
+
+;;; packages.el ends here
